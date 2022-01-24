86275208d9ba5d8d1f8f0b944e54628e2f0aa0be
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-01-24 08:20:54 -0500

    Initial project set up.

---
+++ src/main/java/com/alphacoder/TradeApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeApiGatewayApplication.class, args);
+	}
+
+}
