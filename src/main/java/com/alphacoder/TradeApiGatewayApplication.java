6da674f1f2af8378d0fc6814fc346132829049bf
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-01-24 09:27:50 -0500

    Added Spring cloud gateway impelementation.

--- src/main/java/com/alphacoder/TradeApiGatewayApplication.java
+++ src/main/java/com/alphacoder/TradeApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.alphacoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TradeApiGatewayApplication {
 
 	public static void main(String[] args) {
